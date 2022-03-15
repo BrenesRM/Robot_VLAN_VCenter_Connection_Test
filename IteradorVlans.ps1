07583ae5b731c7f702d5ee677314d508b26dada0
Marlon Brenes <91922207+BrenesRM@users.noreply.github.com>
2022-03-15 11:14:39 -0600

    Add files via upload

---
+++ IteradorVlans.ps1
@@ -0,0 +1,36 @@
+﻿$VLANS = Import-Csv -Path "E:\Scripts\VLANS.csv" -Delimiter ";"
+#$VLANS
+
+foreach($mVlan in $VLANS){
+    Set-NetAdapterAdvancedProperty Ethernet0 -DisplayName "VLAN ID" -DisplayValue $mVlan.VLAN
+    Get-NetAdapter Ethernet0 | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
+    Get-NetAdapter Ethernet0 | Remove-NetRoute -AddressFamily IPv4 -Confirm:$false
+    Get-NetAdapter Ethernet0 | New-NetIPAddress -AddressFamily IPv4 -IPAddress $mVlan.IP -PrefixLength $mVlan.Prefix -DefaultGateway $mVlan.Gateway
+
+    sleep -Seconds 5
+
+    $Gateway = $mVlan.Gateway
+    $VLANindex = $mVlan.VLAN
+
+
+    Invoke-Pester -Script @{ Path = "E:\Scripts\Tests\Ping.test.ps1"; Parameters = @{Gateway = $Gateway; VLAN = $mVlan.VLAN}} -OutputFile "e:\scripts\tests\Result_Test_$VLANindex.xml" -OutputFormat LegacyNUnitXml
+
+
+    
+   #$mVlan
+
+   #Invoke-Pester -Script @{ Path =  'C:\Test.ps1'; Parameters = @{ServerName = 'SRV1'; ServiceName = 'wuauserv'} }
+}
+
+###### Al final deberiamos quedar en la 2068 y mandar el resultado a splunk 
+
+###Proceso para automatizar las pruebas
+
+# VRA pone a maquina 
+# VRO manda a modo mantenimiento EL host Objetivo
+# VRO Apaga DRS y HA en el cluster 
+# VRO inicia VMOTION hacia el host objetivo 
+# Algo dispara Robot
+# : Repita para cada host
+
+# Verifique resultados en splunk 
---
+++ Ping.test.ps1
@@ -0,0 +1,16 @@
+﻿param ($Gateway, $VLAN)
+#$Gateway = "192.168.164.1"
+Describe "Ping al gateway $Gateway" {
+
+ Context "Ping for VLAN $VLAN" {
+
+
+it  "Server ping" {
+
+$Chek_connection = Test-NetConnection -ComputerName $Gateway
+$Chek_connection.PingSucceeded | should be $true
+}
+}
+}
+
+
